9a32a1d36506127cf5fdb4c0417d2329d1621398
anush <anushasree@gmail.com>
2023-02-01 12:39:09 -0500

    adding car file

---
+++ src/main/java/Car.java
@@ -0,0 +1,34 @@
+import java.util.Objects;
+
+public class Car {
+
+
+    String model;
+    String make;
+
+public Car(String carModel, String carMake){
+    this.model= carModel;
+    this.make=carMake;
+}
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", make='" + make + '\'' +
+                '}';
+
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return model.equals(car.model) && make.equals(car.make);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, make);
+    }
+}
