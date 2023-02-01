fe8fe960f2a90d439e4bba246274790703c456c5
anush <anushasree@gmail.com>
2023-02-01 12:39:43 -0500

    adding AdditionExample file

---
+++ src/main/java/AdditionExample.java
@@ -0,0 +1,17 @@
+
+
+
+  public class AdditionExample {
+
+        public static void main(String[] args) {
+            System.out.println("Result:"+addition(7,8));
+            int sum=addition(5,6);
+            String s="Result"+" " +"is" +" "+"very good";
+            System.out.println(s);
+        }
+
+        public static int addition(int a,int b){
+            return a+b;
+        }
+    }
+
