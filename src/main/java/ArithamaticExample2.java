db9a62c8ceb54abe3d9753c1251647da03a2f26e
anush <anushasree@gmail.com>
2023-02-02 08:31:18 -0500

    adding ArithaMaticOperators2 file

---
+++ src/main/java/ArithamaticExample2.java
@@ -0,0 +1,12 @@
+public class ArithamaticExample2 {
+
+    public static void main(String args[]){
+        int a=15;
+        int b=5;
+        //System.out.println(a+b);//15
+        //System.out.println(a-b);//5
+        //System.out.println(a*b);//50
+        //System.out.println(a/b);//2
+        System.out.println(a%b);
+    }
+}
