0e970cbec34ad4f35427047bf7ccfb951f2d8997
anush <anushasree@gmail.com>
2023-09-08 11:29:52 -0400

    adding changes for WhileMultiflicationTable.java

--- src/main/java/HeadFirstJava/WhileMultiflicationTable.java
+++ src/main/java/HeadFirstJava/WhileMultiflicationTable.java
@@ -4,14 +4,15 @@ import com.sun.jdi.Value;
 
 public class WhileMultiflicationTable {
     public static void main(String[] tables) {
-        System.out.println("Number of Command Line Argument = " + tables.length);
-        int index=0;
-        while(index<tables.length) {
-            String value=tables[index];
-            System.out.println(value);
-            index++;
+       // System.out.println("Number of Command Line Argument = " + tables.length);
+       // int index=0;
+       // while(index<tables.length) {
+          //  String value=tables[index];
+           // System.out.println(value);
+          //  index++;
 
-            int number = Integer.parseInt(value);
+           // int number = Integer.parseInt(value);
+        int number=8;
 
             int i = 1;
             while (i <= 10) {
@@ -24,4 +25,4 @@ public class WhileMultiflicationTable {
 
         }
     }
-}
+
