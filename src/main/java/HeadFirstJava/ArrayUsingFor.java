3042348f12ef86370f13c50b4324570caa021167
anush <anushasree@gmail.com>
2023-09-11 13:19:11 -0400

    adding ArrayUsingFor.java

---
+++ src/main/java/HeadFirstJava/ArrayUsingFor.java
@@ -0,0 +1,13 @@
+package HeadFirstJava;
+
+public class ArrayUsingFor {
+    public static void main(String[] args) {
+
+        int[] numbers={5,6,7,-4};
+        for(int number:numbers){
+            System.out.println(number);
+
+        }
+
+    }
+}
